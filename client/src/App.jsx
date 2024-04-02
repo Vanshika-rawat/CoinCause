c46da29795a266adb7f1fe8a4c61a470a97ff8fa
TaanyaTarun <taanyatarun@gmail.com>
2024-04-02 23:28:10 +0530

    clean up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,3 @@
-// import { ConnectWallet } from "@thirdweb-dev/react";
-// import Donor from "./pages/Donor";
-// import Login from "./pages/Login/Login";
-// import Org from "./pages/Org";
-// import Register from "./pages/Register/Register";
-// import { BrowserRouter as Router } from 'react-router-dom';
-
-
-// import {
-//   createBrowserRouter,
-//   RouterProvider
-// } from 'react-router-dom'
-
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <div><Login/></div>
-//   },
-//   {
-//     path: '/register',
-//     element: <div><Register/></div>
-//   },
-//   {
-//     path: '/donor',
-//     element: <div><Donor/></div>
-//   },
-//   {
-//     path: '/org/*',
-//     element: <div><Org/></div>
-//   }
-// ])
-
-// export default function Home() {
-//   return (
-//     <RouterProvider router={router}/>
-//   );
-// }
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ConnectWallet } from "@thirdweb-dev/react";
@@ -43,6 +5,7 @@ import Donor from "./pages/Donor";
 import Login from "./pages/Login/Login";
 import Org from "./pages/Org";
 import Register from "./pages/Register/Register";
+
 
 export default function App() {
   return (
